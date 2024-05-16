e023d956598e20008b87ce81cf38a0773b47a5b8
Alejandro González Zárate <124840787+413C5@users.noreply.github.com>
2024-05-15 20:12:19 -0600

    Basic output to console with typescript

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,18 @@
+//Constant
+const name: string = 'Strider'
+
+const isAlive: boolean = true
+
+//Variable
+let hpPoints: number | string = 10
+
+hpPoints = 'Hola'
+
+console.log({
+    name,
+    hpPoints,
+    isAlive
+})
+
+
+export { }
