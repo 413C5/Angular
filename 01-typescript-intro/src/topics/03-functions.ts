28832ad650b7d429ab2c95f3223ff6abbec9b74b
Alejandro González Zárate <124840787+413C5@users.noreply.github.com>
2024-05-22 21:00:57 -0600

    Functions and interfaces
    Interfaces were implemented alongside functions

---
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -0,0 +1,43 @@
+//When putting a type with a function, we are definyng the return data type
+const addNumbers = (a: number, b: number): Number => {
+    return a + b;
+
+}
+
+const multiply = (a: number, base: number, b?: number): number => {
+    if (b === undefined) {
+        return a * base
+    } else {
+        return a * b * base
+    }
+}
+
+//We can also pass objects as an argument, for that we need to use interfaces
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const strider:Character={
+    name:'Strider',
+    hp:50,
+    showHp(){
+        console.log(`HP is ${this.hp}`)
+    }
+}
+
+const healCharacter = (character: Character, amount: number):void => {
+
+    character.hp += amount
+}
+
+
+
+//console.log(addNumbers(1,2))
+//console.log(multiply(2,2))
+healCharacter(strider,10)
+strider.showHp()
+
+
+export { }
