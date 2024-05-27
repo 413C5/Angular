21e0084d8680daf5e0591abc8bdb7a7068eea7cf
Alejandro González Zárate <124840787+413C5@users.noreply.github.com>
2024-05-27 11:28:05 -0600

    Function destructuring
    Destructuring was applied to functions

---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,44 @@
+interface Product {
+  description: string;
+  price: number;
+}
+
+interface TaxCalculationOptions {
+  tax: number;
+  products: Product[];
+}
+
+const phone: Product = {
+  description: "Nokia A1",
+  price: 150.0,
+};
+
+const tablet: Product = {
+  description: "iPad Air",
+  price: 250.0,
+};
+
+const shoppingCart: Product[] = [phone, tablet];
+
+const tax: number = 0.15;
+
+//Clean code states that a functions must have max 3 arguments
+//const taxCalculation = (options: TaxCalculationOptions): [number, number] => {
+//const taxCalculation = ({tax,products}: TaxCalculationOptions): [number, number] => {
+const taxCalculation = (options: TaxCalculationOptions): [number, number] => {
+  let total = 0;
+  const { tax, products } = options;
+
+  products.forEach(({ price }) => {
+    total += price;
+  });
+
+  return [total, total * tax];
+};
+
+const [total, taxTotal] = taxCalculation({ products: shoppingCart, tax: tax });
+
+console.log("Total ", total);
+console.log("Tax ", taxTotal);
+
+export {};
